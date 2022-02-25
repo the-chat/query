26ee44eec1631d4f5e6912dc5c0e196d0b56c880
the-chat <thechatllc@gmail.com>
2022-02-25 14:28:27 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+type Config = Record<string, string>
+
+// search: x=1&y=2&z=afdsf
+const configFromSearch = (search: string) =>
+  search
+    .split("&")
+    .map((keyVal) => keyVal.split("="))
+    .reduce((config, [key, val]) => ({...config, [key]: val}), {})
+
+// return: x=1&y=2&z=afdsf
+const searchFromConfig = (config: Config) =>
+  Object.entries(config)
+    .reduce((search, [key, val]) => search + "&" + key + "=" + val, "")
+    .slice(1)
+
+const getQuery = (url: string) => {
+  const [, search] = url.split("?")
+
+  return configFromSearch(search)
+}
+
+const setQuery = (
+  url: string,
+  config: Config | ((startConfig: Config) => Config)
+) => {
+  const [path] = url.split("?")
+  const startConfig = getQuery(url)
+
+  return (
+    path +
+    "?" +
+    (typeof config == "function"
+      ? searchFromConfig(config(startConfig))
+      : searchFromConfig({...startConfig, ...config}))
+  )
+}
